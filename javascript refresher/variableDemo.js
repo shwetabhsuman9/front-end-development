3f4c6efcd41d3af9e100b06ae9714a3614d7f89d
Shwetabh Suman <1841012338.i.shwetabhsuman@gmail.com>
2023-11-26 03:36:47 +0530

    Create variableDemo.js

---
+++ javascript refresher/variableDemo.js
@@ -0,0 +1,48 @@
+var a= 40;
+// console.log(a);
+// console.log(a ,"forty" , "chaalis");
+
+var student = {
+     name:"Aman",
+     class:"12",
+     age:18, 
+     salary:null,
+     graduation:undefined
+}
+//  console.log("student details: ",student);
+ var s = "Aman is a \bgood boy";
+//  console.log(s);
+
+var a = 5;
+var b=8;
+// console.log("Addition = ",a+=b); 
+// console.log("subtraction = ",a-=b); //Addition
+// console.log("exponent = ",a**=b);
+// console.log("remainder = ",a%=b);
+
+// console.log("multi = ",a*=b);
+
+// console.log("a>b",a>b);
+// console.log("a<b",a<b);
+// console.log("a<=b",a<=b);
+// console.log("a>=b",a>=b);
+
+// console.log("&&",(10&&b));
+
+// console.log(10||b)
+
+const subscribed = true;
+const plan = 800;
+
+if(subscribed){
+    if(plan <= 499){
+        console.log("he has only HD scren");
+    }
+    if(plan >= 699){
+        console.log("he has ultra hd screen");
+    }
+}
+else{
+    console.log("please subscribe!!!");
+}
+
