d6e69529a8826570cd695aec0d820bacbf5c9ca8
Shwetabh Suman <1841012338.i.shwetabhsuman@gmail.com>
2023-11-28 02:15:33 +0530

    Javascript refresher

---
+++ javascript refresher/minOfThree.js
@@ -0,0 +1,11 @@
+let x = 214;
+let y = 45;
+let z = 59;
+
+if (x < y && x < z) {
+  console.log(x);
+} else if (y < x && y < z) {
+  console.log(y);
+} else {
+  console.log(z);
+}
---
+++ javascript refresher/oddEven.js
@@ -0,0 +1,6 @@
+let x = 31;
+if (x % 2 == 0) {
+  console.log("Even No");
+} else {
+  console.log("odd no.");
+}
