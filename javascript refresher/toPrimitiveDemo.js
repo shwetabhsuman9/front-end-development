095b9a0d74ddf04236e0ab21ea52297b020eeb4c
Shwetabh Suman <1841012338.i.shwetabhsuman@gmail.com>
2023-12-19 02:17:03 +0530

    Create toPrimitiveDemo.js

---
+++ javascript refresher/toPrimitiveDemo.js
@@ -0,0 +1,13 @@
+let obj={a:3};
+console.log(2-obj);  //o/p:Nan
+
+let obj1 = { x:1 ,toString(){return 99}};
+console.log(102-obj1); //o/p :3  //Any key values pair doesn't matter what matters is valueOf and toString
+
+let obj2 = {x:58,valueOf(){
+    return 100
+}}
+console.log(101-obj2); //o/p :1
+
+let obj4 = {x:7 , toString(){return{}}};
+console.log(100-obj4);  //Type error
