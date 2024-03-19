06f664af6d93ba1f4b4554621c4e3c99a998b8ae
Shwetabh Suman <1841012338.i.shwetabhsuman@gmail.com>
2024-03-20 01:28:25 +0530

    Update promise.js

--- javascript refresher/promise.js
+++ javascript refresher/promise.js
@@ -2,10 +2,14 @@ function intRandom(n){
     return Math.floor(Math.random()*n);
 }
  function create(){
+    console.log("entering promise");
     return new Promise(function exec(resolve,reject){
         // for(let i=0;i<10000000;i++){
         //     //
         // }
+
+
+        //Async code
         setTimeout(function exec(){
             let x= intRandom(8);
             if(x%2==0){
@@ -21,3 +25,13 @@ function intRandom(n){
 
  let y = create();
  console.log("promise Value",y);
+
+
+ //consuming a promise
+ y.then(
+    function fullfill(){
+console.log("fullfilled",y);
+ },
+ function rejection(){
+    console.log("rejected",y);
+ })
